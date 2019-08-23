56c287ec4d5e08f53e11cd29f8178dfb557eeb9c
Christer <christer@christerwaters.com>
2019-08-23 22:52:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!-- Get all the portfolio items -->
 <?php
- include '../core/get_top.php';
+ include 'core/get_top.php';
 ?>
 <?php
- include '../core/loop_portfolio.php';
+ include 'core/loop_portfolio.php';
 ?>
 <!-- Get all the pages -->
 <?php
-  include '../core/loop_pages.php';
+  include 'core/loop_pages.php';
 ?>
 <!-- Get everything right before </body> tag -->
 <?php
-  include '../core/get_bottom.php';
+  include 'core/get_bottom.php';
 ?>
