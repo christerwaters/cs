ee1f63f433dc8b8a16b7c3516cd9384386266a5b
Christer <christer@christerwaters.com>
2019-08-23 22:34:33 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<?php
+ include $wtrs_steam_loc.'top.php';
+?>
+<?php
+ include $wtrs_steam_loc.'loop_portfolio.php';
+?>
+<!-- Get all the pages -->
+<?php
+  include $wtrs_steam_loc.'loop_pages.php';
+?>
+<!-- Get everything right before </body> tag -->
+<?php
+  include $wtrs_steam_loc.'bottom.php';
+?>
