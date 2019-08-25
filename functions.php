86efc62580cd1406e0c2ea8d5715fe0b7748e256
Christer <christer@christerwaters.com>
2019-08-25 16:48:38 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,4 +1,128 @@
 <?php
 
+add_theme_support( 'post-thumbnails' ); // add featured image support
+add_theme_support( 'post-formats', array( 'video', 'link', 'image' ) ); // add custom formats
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_5d629db34eec4',
+	'title' => 'Post Settings',
+	'fields' => array(
+		array(
+			'key' => 'field_5d629dbe3b824',
+			'label' => 'Type',
+			'name' => 'type',
+			'type' => 'radio',
+			'instructions' => '',
+			'required' => 1,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'choices' => array(
+				'Text' => 'Text',
+				'Video' => 'Video',
+				'Image' => 'Image',
+				'Website' => 'Website',
+			),
+			'allow_null' => 0,
+			'other_choice' => 0,
+			'default_value' => 'Text',
+			'layout' => 'vertical',
+			'return_format' => 'value',
+			'save_other_choice' => 0,
+		),
+		array(
+			'key' => 'field_5d629de13b825',
+			'label' => 'Video File (mp4)',
+			'name' => 'video_file_mp4',
+			'type' => 'file',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array(
+				array(
+					array(
+						'field' => 'field_5d629dbe3b824',
+						'operator' => '==',
+						'value' => 'Video',
+					),
+				),
+			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'url',
+			'library' => 'all',
+			'min_size' => '',
+			'max_size' => '',
+			'mime_types' => 'mp4,mv4',
+		),
+		array(
+			'key' => 'field_5d629e173b826',
+			'label' => 'Video File (ogg)',
+			'name' => 'video_file_ogg',
+			'type' => 'file',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array(
+				array(
+					array(
+						'field' => 'field_5d629dbe3b824',
+						'operator' => '==',
+						'value' => 'Video',
+					),
+				),
+			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+			'library' => 'all',
+			'min_size' => '',
+			'max_size' => '',
+			'mime_types' => '',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'page',
+			),
+		),
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'portfolio',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'side',
+	'style' => 'seamless',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+endif;
 
 ?>
