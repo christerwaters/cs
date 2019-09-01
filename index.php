18fd45a21261201f1c49608e8cdc6801e47c23a1
Christer <christer@christerwaters.com>
2019-09-01 20:10:38 +0200

    Favicon fix

--- index.php
+++ index.php
@@ -5,23 +5,16 @@
 
 
 
-    <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="<?php bloginfo('template_directory'); ?>/media/favicon/android-icon-192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/media/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_directory'); ?>/media/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('template_directory'); ?>/media/favicon/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_directory'); ?>/media/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php bloginfo('template_directory'); ?>/media/favicon/manifest.json">
-    <meta name="msapplication-TileColor" content="#00f">
-    <meta name="msapplication-TileImage" content="<?php bloginfo('template_directory'); ?>/media/favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#00f">
+    <link rel="manifest" href="<?php bloginfo('template_directory'); ?>/media/favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php bloginfo('template_directory'); ?>/media/favicon/safari-pinned-tab.svg" color="#0000ff">
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/media/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="<?php bloginfo('template_directory'); ?>/media/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#0000ff">
+
 
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/style.css"> <!-- stylesheet for this page -->
 
