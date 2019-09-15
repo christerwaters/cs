0ab28f1a5c297b3d6e695295a00b1698d1a85741
Christer <christer@christerwaters.com>
2019-09-15 16:42:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -215,6 +215,7 @@
 <script src="<?php bloginfo('template_directory'); ?>/core/js/url_handler.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/core/js/scrolled.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/mainJq.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/core/js/tilt.js"></script>
 
 <?php $pages = get_pages();foreach ($pages as $page_data) {$post_slug = $page_data->post_name; echo '<style>.p-open.p-'.  $post_slug . ' .p-wrap#' . $post_slug . '{opacity:1;display:block;}</style>' ;}?>
 
