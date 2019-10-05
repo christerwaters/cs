e854d2424330cb3b686ea967e3d7c35464e7b370
Christer <christer@christerwaters.com>
2019-10-05 11:35:17 +0200

    Update ar-head.php

--- ar-head.php
+++ ar-head.php
@@ -1,6 +1,5 @@
 
-<script src="https://aframe.io/releases/0.9.1/aframe.min.js"></script>
-<script src="https://rawgit.com/jeromeetienne/AR.js/master/aframe/build/aframe-ar.min.js"></script>
-
-<!-- add this script in order to load a large set of 3D model extensions-->
-<script src="https://rawgit.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
+<!-- include A-Frame obviously -->
+<script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+<!-- include ar.js for A-Frame -->
+<script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
