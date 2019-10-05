d9f7eb9996cd77467bfd5385bbdb993665a8f696
Christer <christer@christerwaters.com>
2019-10-05 12:19:18 +0200

    Update ar-head.php

--- ar-head.php
+++ ar-head.php
@@ -1,7 +1,6 @@
-
 <!-- include A-Frame obviously -->
-<script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
+<script src="https://aframe.io/releases/0.5.0/aframe.min.js"></script>>
 <!-- include ar.js for A-Frame -->
-<script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+<script src="https://rawgit.com/jeromeetienne/ar.js/master/aframe/build/aframe-ar.js"></script>
 <!-- include three js -->
-<script> THREEx.ArToolkitContext.baseURL = 'https://rawgit.com/jeromeetienne/ar.js/master/three.js'</script>
+<script>THREEx.ArToolkitContext.baseURL = 'https://rawgit.com/jeromeetienne/ar.js/master/three.js/'</script>
