22a489ca3e74057d6e93e316d259eba47d10c5c6
Christer <christer@christerwaters.com>
2019-10-05 12:12:31 +0200

    Update ar-head.php

--- ar-head.php
+++ ar-head.php
@@ -3,3 +3,5 @@
 <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
 <!-- include ar.js for A-Frame -->
 <script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+<!-- include three js -->
+<script> THREEx.ArToolkitContext.baseURL = 'https://rawgit.com/jeromeetienne/ar.js/master/three.js'</script>
