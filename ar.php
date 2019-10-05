8939db0581993019628b6c9d6e15ae272716cf2a
Christer <christer@christerwaters.com>
2019-10-05 11:21:29 +0200

    Update ar.php

--- ar.php
+++ ar.php
@@ -1,10 +1,4 @@
 
-<script src="https://aframe.io/releases/0.9.1/aframe.min.js"></script>
-<script src="https://rawgit.com/jeromeetienne/AR.js/master/aframe/build/aframe-ar.min.js"></script>
-
-<!-- add this script in order to load a large set of 3D model extensions-->
-<script src="https://rawgit.com/donmccurdy/aframe-extras/master/dist/aframe-extras.loaders.min.js"></script>
-
 <div class="p-wrap" id="ar">
   <!-- include A-Frame obviously -->
   <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
