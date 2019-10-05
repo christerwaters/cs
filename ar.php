f4b5e4948a4a46b81d6a33531d26a6fdfc97e37c
Christer <christer@christerwaters.com>
2019-10-05 12:12:33 +0200

    Update ar.php

--- ar.php
+++ ar.php
@@ -1,10 +1,7 @@
 
 <div class="p-wrap" id="ar">
-  <body style='margin : 0px; overflow: hidden;'>
-    <a-scene embedded arjs>
-      <!-- create your content here. just a box for now -->
-      <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-      <!-- define a camera which will move according to the marker position -->
-      <a-marker-camera preset='hiro'></a-marker-camera>
-    </a-scene>
+  <a-scene embedded artoolkit='sourceType: webcam;'>
+    <a-box position='0 0.5 0' material='color: yellow;'></a-box>
+    <a-marker-camera preset='hiro'></a-marker-camera>
+  </a-scene>
 </div>
