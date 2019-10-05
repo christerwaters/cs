c6a6adc351b7a33e70e4b6ec9a73d6470096d006
Christer <christer@christerwaters.com>
2019-10-05 12:22:26 +0200

    Update ar-head.php

--- ar-head.php
+++ ar-head.php
@@ -1,5 +1,5 @@
 <!-- include A-Frame obviously -->
-<script src="https://aframe.io/releases/0.5.0/aframe.min.js"></script>>
+<script src="https://aframe.io/releases/0.5.0/aframe.min.js"></script>
 <!-- include ar.js for A-Frame -->
 <script src="https://rawgit.com/jeromeetienne/ar.js/master/aframe/build/aframe-ar.js"></script>
 <!-- include three js -->
