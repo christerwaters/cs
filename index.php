23cd39e4b939897ab1ce81ce3a92e3d7e76da39c
Christer <christer@christerwaters.com>
2019-10-07 22:27:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -27,9 +27,7 @@
     <meta property="og:url" content="https://wtrs.dev" />
     <meta property="og:title" content="Christer Waters - WTRS.dev - Freelance Designer & Developer" />
     <meta property="og:description" content="I build human-friendly websites, always optimized for search-engines and any device. I also create artworks in the form of still and moving imagry (video and photo)." />
-
-    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
 
 
 
