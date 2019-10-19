b152b058233d71d3834c99131a6faa183374850b
Christer <christer@christerwaters.com>
2019-10-19 02:18:29 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@ add_theme_support( 'post-formats', array( 'video', 'link', 'image' ) ); // add c
 
 if( function_exists('acf_add_local_field_group') ):
 
-define('WPCF7_LOAD_JS', false);
 
 acf_add_local_field_group(array(
 	'key' => 'group_5d629db34eec4',
