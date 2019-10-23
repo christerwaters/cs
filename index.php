e486299c423067a2f35d64b679cda2591b852c5f
Christer <christer@christerwaters.com>
2019-10-23 20:12:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -229,11 +229,15 @@
   <div class="foot">
     <div class="foot-inner">
       <div class="foot-left">
-        © Citrus Studio
+        <div class="foot-inner">
+          © Citrus Studio
+        </div>
       </div><div class="foot-right">
-        <span class="contact-btn">
-          <a href="<?php echo get_site_url();?>/om-oss#kontakt"> Kontakt Oss</a>
-        </span>
+        <div class="foot-inner">
+          <span class="contact-btn">
+            <a href="<?php echo get_site_url();?>/om-oss#kontakt"> Kontakt Oss</a>
+          </span>
+        </div>
       </div>
     </div>
   </div>
