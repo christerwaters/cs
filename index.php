4093ed251fe4ea9c5553e17f4550e663e919ca54
Christer <christer@christerwaters.com>
2019-10-24 22:11:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
 
   ?><div class="post-wrap <?php foreach($category_detail as $cd){  echo $cd->cat_name; echo " ";} ?>" id="<?php echo $post_slug;?>">
     <a href="<?php echo $post_url;?>" title="<?php echo $title;?>" >
-      <div class="post <?php echo $select_format_type; echo " "; foreach($category_detail as $cd){echo $cd->cat_name; echo " ";} ?> " id="<?php echo $post_slug;?>" <?php if ($select_format_type != ('Text')){ echo 'data-tilt'};?>>
+      <div class="post <?php echo $select_format_type; echo " "; foreach($category_detail as $cd){echo $cd->cat_name; echo " ";} ?> " id="<?php echo $post_slug;?>" <?php if ($select_format_type != ('Text')){?> data-tilt <?php};?>>
       <div class="featured">
         <?php if ($select_format_type == ('Video')){?>
           <video poster="<?php echo $image_full;?>" class="js-player" loop>
