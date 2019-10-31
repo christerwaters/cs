626945e35b1b8ad60d1e139d17ee08d9f0524985
Christer <christer@christerwaters.com>
2019-10-31 17:56:57 +0100

    Update gutenberg.php

--- gutenberg.php
+++ gutenberg.php
@@ -1,4 +1,6 @@
-<?php function wtrs_gutenberg_blocks(){
+<?php
+
+function wtrs_gutenberg_blocks(){
   wp_register_script( 'wtrs-featured-video', get_template_directory_uri() . '/core/js/blocks/wtrs-featured-video.js', array('wp-blocks'));
   register_block_type('wtrs/wtrs-featured-video', array(
     'editor_script' => 'wtrs-featured-video';
