4565041e93862a0d7375122f76d53d6c68bcf961
Christer <christer@christerwaters.com>
2019-10-31 23:39:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 
 $(window).scroll(function() {
   var scroller = $(window).scrollTop();
-  var vhscroller = 100-scroller/8;
+  var vhscroller = scroller/8;
   var rotscroller = scroller*.4;
   $('.fruit-left').css({ transform: 'translate(-5vw, ' + vhscroller + 'vh) rotate(' + rotscroller + 'deg)' });
   $('.fruit-right').css({ transform: 'translate(5vw, ' + vhscroller + 'vh) rotate(-' + rotscroller + 'deg)' });
