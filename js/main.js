0a012dd291b46e6fcad938c46f04eb93d3cc3f87
Christer <christer@christerwaters.com>
2019-11-04 21:37:50 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,9 +19,13 @@ $(window).scroll(function() {
 $(window).bind('scroll', function(){
     var offset = $(document).scrollTop();
     if( offset<=50 ){
-      //$(".featured-media").removeClass('scrolled100');
-    }else {
+    } else {
       $(".featured-media").addClass('scrolled50');
+    }
+    if(offset === 0) {
+      setTimeout(function(){
+        $(".featured-media").removeClass('scrolled50');
+      }, 100);
     }
 });
 
