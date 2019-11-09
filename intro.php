15413589cbda5c063de4d9725ea2e2524011ff7d
Christer <christer@christerwaters.com>
2019-11-09 13:53:03 +0100

    12312

---
+++ intro.php
@@ -0,0 +1,36 @@
+
+  <div class="intro unloaded">
+    <div class="intro--text-wrap">
+      <div class="intro--text"><span class="c">CITRUS</span> <span class="s">STUDIO</span></div>
+    </div>
+    <div class="fruit" id="one">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
+    </div>
+    <div class="fruit" id="two">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-02.png" >
+    </div>
+    <div class="fruit" id="three">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
+    </div>
+    <div class="fruit" id="four">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-03.png" >
+    </div>
+    <div class="fruit" id="five">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-02.png" >
+    </div>
+    <div class="fruit" id="six">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
+    </div>
+    <div class="fruit" id="seven">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-02.png" >
+    </div>
+    <div class="fruit" id="eight">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
+    </div>
+    <div class="fruit" id="nine">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-03.png" >
+    </div>
+    <div class="fruit" id="ten">
+      <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
+    </div>
+  </div>
