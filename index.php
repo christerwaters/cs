5b542573ea7f2b5e21df66f61164b648717ec863
Christer <christer@christerwaters.com>
2019-11-10 14:34:22 +0100

    321

--- index.php
+++ index.php
@@ -33,37 +33,6 @@
   </head>
 
   <body class="unloaded">
-    <?php include 'intro.php';?>
-    <div class="fruits" data-wtrs-show-on="home">
-      <div class="fruit fruit-left" id="one">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
-      </div>
-      <div class="fruit fruit-left" id="two">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-02.png" >
-      </div>
-      <div class="fruit fruit-left" id="three">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
-      </div>
-      <div class="fruit fruit-left" id="four">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-03.png" >
-      </div>
-      <div class="fruit fruit-right" id="one">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-02.png" >
-      </div>
-      <div class="fruit fruit-right" id="two">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
-      </div>
-      <div class="fruit fruit-right" id="three">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-03.png" >
-      </div>
-      <div class="fruit fruit-right" id="four">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-01.png" >
-      </div>
-      <div class="fruit fruit-right" id="five">
-        <img src="<?php bloginfo('template_directory'); ?>/media/citrus-fruit-02.png" >
-      </div>
-    </div>
-
     <div class="menu">
       <div class="menu-overlay"></div>
       <div class="logotype menu-hover-trigger">
