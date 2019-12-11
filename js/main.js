0a57c1799df85f2ab77e814389a3de2127349ea8
Christer <christer@christerwaters.com>
2019-12-11 21:35:57 +0100

    test

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@ $( ".burger" ).click(function() {
   $( ".menu" ).toggleClass("open");
   $( "body" ).toggleClass("menu-open");
 });
+const players = Array.from(document.querySelectorAll(".js-player")).map(p => new Plyr(p));
