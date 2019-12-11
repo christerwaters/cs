7cf2329869354bb63e6fdf8d39f36d10b1702332
Christer <christer@christerwaters.com>
2019-12-11 20:57:09 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,18 +4,16 @@ add_theme_support( 'post-thumbnails' ); // add featured image support
 add_theme_support( 'post-formats', array( 'video', 'link', 'image' ) ); // add custom formats
 
 
-function cs_enqueue_scripts(){
-  wp_register_script( 'plyr_js', bloginfo('template_directory') . '/js/plyr.js', null, null, true );
+function cs_imp(){
+  wp_register_script( 'plyr_js', get_template_directory_uri() .'/js/plyr.js', null, false, false ); // .'/js/plyr.js', null, null, true );
   wp_enqueue_script('plyr_js');
-  wp_register_script( 'main_js', bloginfo('template_directory') . '/js/main.js', null, null, true );
+  wp_register_script( 'main_js', get_template_directory_uri() .'/js/main.js', null, false, false );
   wp_enqueue_script('main_js');
-  wp_register_style( 'plyr_css', bloginfo('template_directory') . '/css/plyr.css' );
+  wp_register_style( 'plyr_css', get_template_directory_uri() .'/css/plyr.css' );
   wp_enqueue_style('plyr_css');
-  wp_register_style( 'single_css', bloginfo('template_directory') . '/css/single.css' );
+  wp_register_style( 'single_css', get_template_directory_uri() .'/css/single.css' );
   wp_enqueue_style('single_css');
 }
-add_action( 'wp_enqueue_scripts', 'cs_enqueue_scripts' );
-
-
+add_action( 'wp_enqueue_scripts', 'cs_imp' );
 
 ?>
