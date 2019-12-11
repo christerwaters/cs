462eb4aba125b69f2be108d7b2d6aa4ccb2c3998
Christer <christer@christerwaters.com>
2019-12-11 21:40:44 +0100

    MORE LIFE!!!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+const players = Array.from(document.querySelectorAll("video")).map(p => new Plyr(p));
+
+
 $( ".burger" ).click(function() {
   $( ".menu" ).toggleClass("open");
   $( "body" ).toggleClass("menu-open");
 });
-const players = Array.from(document.querySelectorAll(".js-player")).map(p => new Plyr(p));
