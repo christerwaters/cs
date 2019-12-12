b12369d40251100a1b2a3c38336e5b26a3289818
Christer <christer@christerwaters.com>
2019-12-12 15:58:58 +0100

    Commit

--- footer.php
+++ footer.php
@@ -2,3 +2,5 @@
  <script src="<?php bloginfo('template_directory'); ?>/js/plyr.js"></script>
  <script src="<?php bloginfo('template_directory'); ?>/js/jq.js"></script>
  <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
+
+ <?php wp_footer(); ?>
