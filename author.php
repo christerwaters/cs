2a6f4464b4d9d07f0cc8e4ba505dbebb065a8035
Christer <christer@christerwaters.com>
2019-12-12 17:50:29 +0100

    Update author.php

--- author.php
+++ author.php
@@ -4,6 +4,7 @@
 get_header();
 // Set the Current Author Variable $curauth
 $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+$curauthID = $curauth->ID;
 ?>
 
 
@@ -11,6 +12,36 @@ $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : g
 <div class="header">
   <h1><?php echo $curauth->display_name; ?></h1>
 </div>
+
+<?php
+
+$posts = get_posts(array(
+	'posts_per_page'	=> -1,
+	'post_type'			=> 'portfolio',
+	'credits_username'		=> $curauthID
+));
+
+if( $posts ): ?>
+
+	<ul>
+
+	<?php foreach( $posts as $post ):
+
+		setup_postdata( $post );
+
+		?>
+		<li>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		</li>
+
+	<?php endforeach; ?>
+
+	</ul>
+
+	<?php wp_reset_postdata(); ?>
+
+<?php endif; ?>
+
 
 <?php
 get_sidebar();
