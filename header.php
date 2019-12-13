8192242bcfd7a6dc5ec8403710d6a345fb05fac3
Christer <christer@christerwaters.com>
2019-12-13 15:39:29 +0100

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,11 @@
 
 
   <body class="<?php global $post;echo $post->post_name;?>">
-    <a href="<?php echo get_home_url;?>"<div class="logo"><img src=""></div></a>
+    <a href="<?php echo get_home_url;?>">
+      <div class="logo">
+        <img src="<?php if ( function_exists( 'the_custom_logo' ) ) { the_custom_logo();}?>">
+      </div>
+    </a>
     <div class="menu">
       <div class="duvet"></div>
       <a class="burger">
