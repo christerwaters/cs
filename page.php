4efb25def10a3325c492306ba64ec15e92e16d3e
Christer <christer@christerwaters.com>
2019-12-13 13:50:38 +0100

    Update page.php

--- page.php
+++ page.php
@@ -1,35 +1,32 @@
 <?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package lemon
- */
-
-get_header();
+ get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+<div class="header">
 
 <?php
-get_sidebar();
-get_footer();
+	// Start the Loop.
+while ( have_posts() ) :
+	the_post();
+	/*
+	 * Include the post format-specific template for the content. If you want to
+	 * use this in a child theme, then include a file called content-___.php
+	 * (where ___ is the post format) and that will be used instead.
+	 */
+?>
+<h1>
+  <?php the_title(); ?>
+</h1>
+<p class="excerpt">
+  <?php
+    the_excerpt();
+  ?>
+</p>
+</div>
+<?php
+  the_content();
+	endwhile;
+?>
+<?php
+  get_footer();
+?>
