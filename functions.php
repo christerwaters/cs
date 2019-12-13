83884b1d655b8cb198199ea6bdb47d8d6cb52675
Christer <christer@christerwaters.com>
2019-12-13 15:39:27 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 add_theme_support( 'post-thumbnails' ); // add featured image support
 add_theme_support( 'post-formats', array( 'video', 'link', 'image' ) ); // add custom formats
 add_post_type_support( 'page', 'excerpt' );
+add_theme_support( 'custom-logo' );
 
 function cs_imp(){
   wp_register_script( 'plyr_js', get_template_directory_uri() .'/js/plyr.js', null, false, true ); // .'/js/plyr.js', null, null, true );
