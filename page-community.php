1348b749e61f0a70dd342cfc26572b61fb1df561
Christer <christer@christerwaters.com>
2019-12-15 20:53:32 +0100

    css

--- page-community.php
+++ page-community.php
@@ -1,8 +1,28 @@
 <?php
-
-get_header();
-
+ get_header();
 ?>
+
+<div class="header">
+
+<?php
+	// Start the Loop.
+while ( have_posts() ) :
+	the_post();
+?>
+	<?php if ( get_field('hide_title')!='True'){?>
+	<h1>
+	  <?php the_title(); ?>
+	</h1>
+	<?php }; ?>
+
+	<?php if (get_field('hide_excerpt')!='True'){?>
+	<div class="excerpt">
+	  <?php
+	    the_excerpt();
+	  ?>
+	</div>
+	<?php };  ?>
+</div>
 
 <div class="dir">
 
