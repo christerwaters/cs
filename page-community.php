42182e8ac1f8da52a5d31aa1c61f257287234135
Christer <christer@christerwaters.com>
2019-12-15 19:02:51 +0100

    Update page-community.php

--- page-community.php
+++ page-community.php
@@ -11,7 +11,7 @@ get_header();
 $users = get_users();
 foreach($users as $user){
 ?>
-  <div class="dir-item team-item" style="background-image:url(<?php echo get_field('profilepic'); ?>);">
+  <div class="dir-item team-item" style="background-image:url(<?php <?php the_field('profilepic', 'user_'. $user->ID); ?>; ?>);">
     <div class="info">
       <h3>
         <?php echo $user->display_name; ?>
