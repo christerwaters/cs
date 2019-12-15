c9054a5a7571595a0123686a73edc8fab6a96266
Christer <christer@christerwaters.com>
2019-12-15 19:09:21 +0100

    Update page-community.php

--- page-community.php
+++ page-community.php
@@ -11,17 +11,17 @@ get_header();
 $users = get_users();
 foreach($users as $user){
 ?>
-<a href="<?php the_author_posts_url($user->ID);?>">
-  <div class="dir-item team-item" style="background-image:url(<?php the_field('profilepic', 'user_'. $user->ID); ?>);">
-    <div class="info">
-      <div class="info-c">
-        <h3>
-          <?php echo $user->display_name; ?>
-        </h3>
+  <a href="<?php the_author_posts_url($user->ID);?>">
+    <div class="dir-item team-item" style="background-image:url(<?php the_field('profilepic', 'user_'. $user->ID); ?>);">
+      <div class="info">
+        <div class="info-m">
+          <h3>
+            <?php echo $user->display_name; ?>
+          </h3>
+        </div>
       </div>
     </div>
-  </div>
-</a>
+  </a>
 <?php
 }
 
