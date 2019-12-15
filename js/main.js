4f9f011b26429ce589c56f90e50cbdfb669c7140
Christer <christer@christerwaters.com>
2019-12-15 03:32:08 +0100

    :)

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ $( ".burger" ).click(function() {
 
 $("a").hover(function() {
   var bg = $(this).css('background-image').replace('jpg','gif');
-  console.log(bg);
+  var bg = $(this).css('background-image', bg);
+  var bg = $(this).css('background-image').replace('gif','jpg');
   var bg = $(this).css('background-image', bg);
 });
