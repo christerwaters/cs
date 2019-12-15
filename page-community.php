cf57256a9b444edb11e6450055bbcecf88435721
Christer <christer@christerwaters.com>
2019-12-15 18:35:47 +0100

    css

--- page-community.php
+++ page-community.php
@@ -2,38 +2,26 @@
 
 get_header();
 
-
-$allUsers = get_users();
-
-$users = array();
-// Remove subscribers from the list as they won't write any articles
-foreach($allUsers as $currentUser)
-{
-	if(in_array( 'member', $currentUser->roles ))
-	{
-		$users[] = $currentUser;
-	}
-}
 ?>
 
 <div class="dir">
+  
 <?php
-  foreach($users as $user)
-  {
-	?>
 
-  <div class="dir-item portfolio-item" style="background-image:url(<?php the_field('profilepic'); ?>);">
+$users = get_users( array( 'fields' => array( 'ID' ) ) );
+foreach($users as $user_id){
+?>
+  <div class="dir-item team-item" style="background-image:url(<?php the_field('profilepic'); ?>);">
     <div class="info">
       <h3>
         <?php echo $user->display_name; ?>
       </h3>
     </div>
   </div>
+<?php
+}
 
-  <?php
-  }
 ?>
-
 </div>
 <?php
 get_sidebar();
