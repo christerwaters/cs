029bab11129463b0616c03d7fc41e715ae67857f
Christer <christer@christerwaters.com>
2019-12-15 17:43:32 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
-    <script src="<?php bloginfo('template_directory'); ?>/slick/slick.min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/plyr.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/jq.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/slick/slick.min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 
     <?php wp_footer(); ?>
