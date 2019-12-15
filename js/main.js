914c3dbfa988741b55405d53e11e80f05c77cb8a
Christer <christer@christerwaters.com>
2019-12-15 18:56:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,25 +11,5 @@ $( ".burger" ).click(function() {
 $('.blocks-gallery-grid').slick({
   centerMode: true,
   centerPadding: '60px',
-  slidesToShow: 3,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 3
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
+  slidesToShow: 3
 });
