e0599d2ec2be2401da1a573d2a9c94e2e48e072a
Christer <christer@christerwaters.com>
2019-12-15 16:23:38 +0100

    test

--- page-home.php
+++ page-home.php
@@ -30,19 +30,18 @@ while ( have_posts() ) :
 $args = array('post_type' => 'portfolio'); //declares that we will only be querying the portfolio post type
 $portfolio_items = get_posts( $args );
 foreach ($portfolio_items as $page_data) {
-    $content = apply_filters('the_content', $page_data->post_content);
     $title = $page_data->post_title;
-    $post_slug = $page_data->post_name;
+    $perm = get_permalink($page_data->ID);
     $imageid_full = wp_get_attachment_image_src( get_post_thumbnail_id($page_data->ID), 'full' );
     $image_full = $imageid_full['0'];
 ?>
         <!-- LAY OUT THE TEMPLATE FOR IMAGES HERE -->
 
           <a href="<?php the_permalink(); ?>">
-            <div class="dir-item portfolio-item" style="background-image:url(<?php echo get_the_post_thumbnail_url(null, 'full'); ?>);">
+            <div class="dir-item portfolio-item" style="background-image:url(<?php echo $image_full; ?>);">
               <div class="info">
                 <h3>
-                  <?php the_title(); ?>
+                  <?php echo $title; ?>
                 </h3>
               </div>
             </div>
