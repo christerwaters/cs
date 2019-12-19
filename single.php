06fd538178a89e9ce442a61af8fe5311e734020e
Christer <christer@christerwaters.com>
2019-12-19 22:27:02 +0100

    Update single.php

--- single.php
+++ single.php
@@ -36,9 +36,10 @@ while ( have_posts() ) :
             <?php the_sub_field('extname')?>.
           <?php }; ?>
           <?php if( get_sub_field('intname') ) { ?>
-              <?php $user = get_user_by( 'ID', get_sub_field('intname') );
-                    echo $user->display_name;
-              ?>
+            <?php echo 'proof'; ?>
+            <?php $user = get_user_by( 'ID', get_sub_field('intname') );
+                  echo $user->display_name;
+            ?>
             <a href="<?php echo get_author_posts_url(get_sub_field('intname'));?>">
             </a>
           <?php }; ?>
