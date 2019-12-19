4698981b818b1827fd78481b0fa504a35b54c891
Christer <christer@christerwaters.com>
2019-12-19 07:10:26 +0100

    yeah

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/style.css"> <!-- stylesheet for this page -->
+  <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
 
 	<?php wp_head(); ?>
 </head>
--- js/main.js
+++ js/main.js
@@ -23,3 +23,12 @@ for (var i = 0; i < accordions.length; i++) {
     }
   }
 }
+
+
+var randomize = $(".team-item");
+
+for(var i = 0; i < randomize.length; i++){
+    var target = Math.floor(Math.random() * randomize.length -1) + 1;
+    var target2 = Math.floor(Math.random() * randomize.length -1) +1;
+    randomize.eq(target).before(randomize.eq(target2));
+}
