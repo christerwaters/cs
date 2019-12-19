60e324a7db83d82758f619387de33f1cd6de2cdf
Christer <christer@christerwaters.com>
2019-12-19 16:05:58 +0100

    tea

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ for (var i = 0; i < accordions.length; i++) {
 }
 
 
-var randomize = $(".team-item");
+var randomize = $(".community .dir a");
 
 for(var i = 0; i < randomize.length; i++){
     var target = Math.floor(Math.random() * randomize.length -1) + 1;
