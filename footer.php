3b0d2d9e444265e1efb28fd5bf58db6e14d0a8fa
Christer <christer@christerwaters.com>
2019-12-25 21:20:46 +0100

    test

--- footer.php
+++ footer.php
@@ -1,8 +1,4 @@
 
-    <script src="<?php bloginfo('template_directory'); ?>/js/plyr.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/js/jq.js"></script>
-    <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
-
     <?php wp_footer(); ?>
   </body>
 </html>
