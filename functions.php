eafbefb359e8fb8d8e21ac730e66024d0d5d983a
Christer <christer@christerwaters.com>
2019-12-25 21:42:41 +0100

    popa

--- functions.php
+++ functions.php
@@ -83,7 +83,7 @@ function cs_imp(){
   wp_register_script( 'jq', get_template_directory_uri() .'/js/jq.js', null, false, true );
   wp_enqueue_script('jq_js');
   wp_register_script( 'main_js', get_template_directory_uri() .'/js/main.js', 'jq_js', false, false );
-  wp_enqueue_script('main_js');
+  wp_enqueue_script('main_js', array('jq_js'));
   wp_register_style( 'plyr_css', get_template_directory_uri() .'/css/style.css' );
   wp_enqueue_style('plyr_css');
 }
