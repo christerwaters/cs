801917eb46562d43cfc534c4112bb17291b479f6
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-02-12 16:52:26 +0100

    ...

--- header.php
+++ header.php
@@ -14,8 +14,7 @@
 	<?php wp_head(); ?>
 </head>
   <body class="<?php global $post;echo $post->post_name;?>">
-
-		<a href="<?php __previous_url(); ?>">WHYYYYYYY<div class="back-button"></div></a>
+		<?php if (_previous_url() ){?><a href="<?php __previous_url(); ?>"><div class="back"></div></a><?php }; ?>
     <div class="logo">
       <?php the_custom_logo();?>
     </div>
