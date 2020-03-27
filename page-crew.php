b342ee0d3135225f5b1b77e55623fe62eb04e3ca
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-03-27 20:12:10 +0100

    ...

--- page-crew.php
+++ page-crew.php
@@ -43,7 +43,7 @@ foreach($users as $user){
 
 <?php if ( get_field('profilepic', 'user_'. $user->ID)){?>
 <span class="tilfeldig">
-  <?php echo user_url($user->ID);?>
+  <?php get_the_author_meta('user_url',$user->ID);?>
     <div class="dir-item team-item" style="background-image:url(<?php the_field('profilepic', 'user_'. $user->ID); ?>);">
       <div class="info">
         <div class="info-m">
