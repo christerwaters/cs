e25a6406b38c5ea63c88dcf3da61a643ea7cdc22
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-04-26 11:47:59 +0200

    11

--- js/main.js
+++ js/main.js
@@ -60,3 +60,88 @@ function checkSession(){
     body.classList.add("firsttimer");
   }
 }
+
+
+
+
+var timeoutid = 0;
+$(document).on("mousemove", function (e) {
+  var xPos = e.pageX;
+  var yPos = e.pageY;
+  // console.log(xPos, yPos);
+  $("p").css({
+    top: yPos,
+    left: xPos
+  });
+});
+
+var flipperarr = new Array();
+$.ajax({
+  url: "/test/",
+  success: function(data){
+     $(data).find("td > a").each(function(){
+        if(openFile($(this).attr("href"))){
+            fileNames.push($(this).attr("href"));
+        }
+     });
+  }
+});
+console.log(flipperarr);
+function openFile(file) {
+    var extension = file.substr( (file.lastIndexOf('.') +1) );
+    switch(extension) {
+        case 'jpg':
+        case 'png':
+        case 'gif':   // the alert ended with pdf instead of gif.
+        case 'zip':
+        case 'rar':
+        case 'pdf':
+        case 'php':
+        case 'doc':
+        case 'docx':
+        case 'xls':
+        case 'xlsx':
+            return true;
+            break;
+        default:
+            return false;
+    }
+};
+var arr = flipperarr;
+shuffle(arr);
+
+// run through the array forever
+(function recurse(counter) {
+  // get the colour
+  var color = arr[counter];
+  // animate it
+  $(".flipper").css("background-image", "url(/" + color + ")");
+  delete arr[counter];
+  // add the value at the end of the array
+  arr.push(color);
+  // run it again for the next number
+  setTimeout(function () {
+    recurse(counter + 1);
+  }, 200);
+  // start it for the first number.
+})(0);
+
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
