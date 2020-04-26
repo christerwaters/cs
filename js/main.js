1d016f4e4c7c76f49eb756f748db5d01c52b3161
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-04-26 10:53:10 +0200

    1

--- js/main.js
+++ js/main.js
@@ -43,3 +43,19 @@ for(var i = 0; i < randomize.length; i++){
     var target2 = Math.floor(Math.random() * randomize.length -1) +1;
     randomize.eq(target).before(randomize.eq(target2));
 }
+
+function setCookie(c_name,value,exdays){var exdate=new Date();exdate.setDate(exdate.getDate() + exdays);var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());document.cookie=c_name + "=" + c_value;}
+
+function getCookie(c_name){var c_value = document.cookie;var c_start = c_value.indexOf(" " + c_name + "=");if (c_start == -1){c_start = c_value.indexOf(c_name + "=");}if (c_start == -1){c_value = null;}else{c_start = c_value.indexOf("=", c_start) + 1;var c_end = c_value.indexOf(";", c_start);if (c_end == -1){c_end = c_value.length;}c_value = unescape(c_value.substring(c_start,c_end));}return c_value;}
+
+checkSession();
+
+function checkSession(){
+   var c = getCookie("visited");
+   if (c === "yes") {
+     alert("Welcome back!");
+   } else {
+     setCookie("visited", "yes", 365);
+     alert("Welcome new visitor!");
+   }
+}
