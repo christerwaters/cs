b9519e63fcfb6ab43595186ce10a315a02d55161
christerwaters <42318403+christerwaters@users.noreply.github.com>
2020-04-27 13:06:37 +0200

    11

--- flipper.php
+++ flipper.php
@@ -59,3 +59,55 @@
   top: 42%;
 }
 </style>
+<?php include("flipper_b64.php");?>
+<script>
+var arr = [b64_001, b64_002, b64_003, b64_004, b64_005, b64_006, b64_007, b64_008, b64_009, b64_010];
+
+           var timeoutid = 0;
+           $(document).on("mousemove", function (e) {
+             var xPos = e.pageX;
+             var yPos = e.pageY;
+             // console.log(xPos, yPos);
+             $(".flipper-text").css({
+               top: yPos,
+               left: xPos
+             });
+           });
+           shuffle(arr);
+           // run through the array forever
+           (function recurse(counter) {
+             // get the colour
+             var color = arr[counter];
+             // animate it
+             $(".flipper").css("background-image", "url(" + color + ")");
+             delete arr[counter];
+             // add the value at the end of the array
+             arr.push(color);
+             // run it again for the next number
+             setTimeout(function () {
+               recurse(counter + 1);
+             }, 200);
+             // start it for the first number.
+           })(0);
+
+           function shuffle(array) {
+             var currentIndex = array.length,
+               temporaryValue,
+               randomIndex;
+
+             // While there remain elements to shuffle...
+             while (0 !== currentIndex) {
+               // Pick a remaining element...
+               randomIndex = Math.floor(Math.random() * currentIndex);
+               currentIndex -= 1;
+
+               // And swap it with the current element.
+               temporaryValue = array[currentIndex];
+               array[currentIndex] = array[randomIndex];
+               array[randomIndex] = temporaryValue;
+             }
+
+             return array;
+           }
+
+</script>
